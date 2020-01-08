3fd25b58c0230347c8591e9df149f10e273d38e2
Stephan Rasp <raspstephan@gmail.com>
2020-01-08 15:18:38 +0100

    Extended data generators to work with multi-level files

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from src import score
 from src import train_nn
+from src import clr
--- src/train_nn.py
+++ src/train_nn.py
@@ -1,5 +1,6 @@
 from src.score import *
 import os
+import ast, re
 import numpy as np
 import xarray as xr
 import tensorflow as tf
@@ -7,6 +8,7 @@ import tensorflow.keras as keras
 from tensorflow.keras.layers import Input, Dropout, Conv2D, Lambda
 import tensorflow.keras.backend as K
 from configargparse import ArgParser
+import datetime
 
 def limit_mem():
     config = tf.ConfigProto()
@@ -15,7 +17,8 @@ def limit_mem():
 
 
 class DataGenerator(keras.utils.Sequence):
-    def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True, mean=None, std=None):
+    def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True,
+                 mean=None, std=None, output_vars=None):
         """
         Data generator for WeatherBench data.
         Template from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
@@ -29,7 +32,7 @@ class DataGenerator(keras.utils.Sequence):
             mean: If None, compute mean from data.
             std: If None, compute standard deviation from data.
         """
-
+        print('DG start', datetime.datetime.now().time())
         self.ds = ds
         self.var_dict = var_dict
         self.batch_size = batch_size
@@ -37,18 +40,40 @@ class DataGenerator(keras.utils.Sequence):
         self.lead_time = lead_time
 
         data = []
+        level_names = []
         generic_level = xr.DataArray([1], coords={'level': [1]}, dims=['level'])
-        for var, levels in var_dict.items():
-            try:
-                data.append(ds[var].sel(level=levels))
-            except ValueError:
-                data.append(ds[var].expand_dims({'level': generic_level}, 1))
+        for long_var, params in var_dict.items():
+            if long_var == 'constants':
+                for var in params:
+                    data.append(ds[var].expand_dims(
+                        {'level': generic_level, 'time': ds.time}, (1, 0)
+                    ))
+                    level_names.append(var)
+            else:
+                var, levels = params
+                try:
+                    data.append(ds[var].sel(level=levels))
+                    level_names += [f'{var}_{level}' for level in levels]
+                except ValueError:
+                    data.append(ds[var].expand_dims({'level': generic_level}, 1))
+                    level_names.append(var)
 
         self.data = xr.concat(data, 'level').transpose('time', 'lat', 'lon', 'level')
-        self.mean = self.data.mean(('time', 'lat', 'lon')).compute() if mean is None else mean
-        self.std = self.data.std('time').mean(('lat', 'lon')).compute() if std is None else std
+        self.data['level_names'] = xr.DataArray(
+            level_names, dims=['level'], coords={'level': self.data.level})
+        if output_vars is None:
+            self.output_idxs = range(len(self.data.level))
+        else:
+            self.output_idxs = [i for i, l in enumerate(self.data.level_names.values)
+                                if any([bool(re.match(o, l)) for o in output_vars])]
+
         # Normalize
+        print('DG normalize', datetime.datetime.now().time())
+        self.mean = self.data.mean(('time', 'lat', 'lon')).compute() if mean is None else mean
+        #         self.std = self.data.std('time').mean(('lat', 'lon')).compute() if std is None else std
+        self.std = self.data.std(('time', 'lat', 'lon')).compute() if std is None else std
         self.data = (self.data - self.mean) / self.std
+
         self.n_samples = self.data.isel(time=slice(0, -lead_time)).shape[0]
         self.init_time = self.data.isel(time=slice(None, -lead_time)).time
         self.valid_time = self.data.isel(time=slice(lead_time, None)).time
@@ -56,7 +81,9 @@ class DataGenerator(keras.utils.Sequence):
         self.on_epoch_end()
 
         # For some weird reason calling .load() earlier messes up the mean and std computations
+        print('DG load', datetime.datetime.now().time())
         if load: print('Loading data into RAM'); self.data.load()
+        print('DG done', datetime.datetime.now().time())
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -66,7 +93,7 @@ class DataGenerator(keras.utils.Sequence):
         'Generate one batch of data'
         idxs = self.idxs[i * self.batch_size:(i + 1) * self.batch_size]
         X = self.data.isel(time=idxs).values
-        y = self.data.isel(time=idxs + self.lead_time).values
+        y = self.data.isel(time=idxs + self.lead_time, level=self.output_idxs).values
         return X, y
 
     def on_epoch_end(self):
@@ -111,65 +138,66 @@ def build_cnn(filters, kernels, input_shape, activation='elu', dr=0):
 
 def create_predictions(model, dg):
     """Create non-iterative predictions"""
-    preds = model.predict_generator(dg)
+    preds = xr.DataArray(
+        model.predict_generator(dg),
+        dims=['time', 'lat', 'lon', 'level'],
+        coords={'time': dg.valid_time, 'lat': dg.data.lat, 'lon': dg.data.lon,
+                'level': dg.data.isel(level=dg.output_idxs).level,
+                'level_names': dg.data.isel(level=dg.output_idxs).level_names
+                },
+    )
     # Unnormalize
-    preds = preds * dg.std.values + dg.mean.values
+    preds = (preds * dg.std.isel(level=dg.output_idxs).values +
+             dg.mean.isel(level=dg.output_idxs).values)
+
     das = []
-    lev_idx = 0
-    for var, levels in dg.var_dict.items():
-        if levels is None:
-            das.append(xr.DataArray(
-                preds[:, :, :, lev_idx],
-                dims=['time', 'lat', 'lon'],
-                coords={'time': dg.valid_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon},
-                name=var
-            ))
-            lev_idx += 1
-        else:
-            nlevs = len(levels)
-            das.append(xr.DataArray(
-                preds[:, :, :, lev_idx:lev_idx+nlevs],
-                dims=['time', 'lat', 'lon', 'level'],
-                coords={'time': dg.valid_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon, 'level': levels},
-                name=var
-            ))
-            lev_idx += nlevs
+    for long_var, params in dg.var_dict.items():
+        if not long_var == 'constants':
+            var, levels = params
+            var_names = [var] if levels is None else [f'{var}_{level}' for level in levels]
+            var_idxs = [i for i, v in enumerate(preds.level_names) if v in var_names]
+            da = preds.isel(level=var_idxs)
+            if levels is not None:
+                da = da.sel(level=levels).drop('level_names')
+            else:
+                da = da.squeeze().drop('level').drop('level_names')
+            das.append({var: da})
     return xr.merge(das)
 
-
-def create_iterative_predictions(model, dg, max_lead_time=5 * 24):
-    """Create iterative predictions"""
-    state = dg.data[:dg.n_samples]
-    preds = []
-    for _ in range(max_lead_time // dg.lead_time):
-        state = model.predict(state)
-        p = state * dg.std.values + dg.mean.values
-        preds.append(p)
-    preds = np.array(preds)
-
-    lead_time = np.arange(dg.lead_time, max_lead_time + dg.lead_time, dg.lead_time)
-    das = []
-    lev_idx = 0
-    for var, levels in dg.var_dict.items():
-        if levels is None:
-            das.append(xr.DataArray(
-                preds[:, :, :, :, lev_idx],
-                dims=['lead_time', 'time', 'lat', 'lon'],
-                coords={'lead_time': lead_time, 'time': dg.init_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon},
-                name=var
-            ))
-            lev_idx += 1
-        else:
-            nlevs = len(levels)
-            das.append(xr.DataArray(
-                preds[:, :, :, :, lev_idx:lev_idx + nlevs],
-                dims=['lead_time', 'time', 'lat', 'lon', 'level'],
-                coords={'lead_time': lead_time, 'time': dg.init_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon,
-                        'level': levels},
-                name=var
-            ))
-            lev_idx += nlevs
-    return xr.merge(das)
+# TODO: Outdated
+# def create_iterative_predictions(model, dg, max_lead_time=5 * 24):
+#     """Create iterative predictions"""
+#     state = dg.data[:dg.n_samples]
+#     preds = []
+#     for _ in range(max_lead_time // dg.lead_time):
+#         state = model.predict(state)
+#         p = state * dg.std.values + dg.mean.values
+#         preds.append(p)
+#     preds = np.array(preds)
+#
+#     lead_time = np.arange(dg.lead_time, max_lead_time + dg.lead_time, dg.lead_time)
+#     das = []
+#     lev_idx = 0
+#     for var, levels in dg.var_dict.items():
+#         if levels is None:
+#             das.append(xr.DataArray(
+#                 preds[:, :, :, :, lev_idx],
+#                 dims=['lead_time', 'time', 'lat', 'lon'],
+#                 coords={'lead_time': lead_time, 'time': dg.init_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon},
+#                 name=var
+#             ))
+#             lev_idx += 1
+#         else:
+#             nlevs = len(levels)
+#             das.append(xr.DataArray(
+#                 preds[:, :, :, :, lev_idx:lev_idx + nlevs],
+#                 dims=['lead_time', 'time', 'lat', 'lon', 'level'],
+#                 coords={'lead_time': lead_time, 'time': dg.init_time, 'lat': dg.ds.lat, 'lon': dg.ds.lon,
+#                         'level': levels},
+#                 name=var
+#             ))
+#             lev_idx += nlevs
+#     return xr.merge(das)
 
 def create_cnn(filters, kernels, dropout=0., activation='elu', periodic=True):
     assert len(filters) == len(kernels), 'Requires same number of filters and kernel_sizes.'
@@ -190,34 +218,29 @@ def create_cnn(filters, kernels, dropout=0., activation='elu', periodic=True):
     return model
 
 
-def main(datadir, vars, filters, kernels, lr, activation, dr, batch_size, patience, model_save_fn, pred_save_fn,
-         train_years, valid_years, test_years, lead_time, gpu, iterative):
+def main(datadir, var_dict, output_vars, filters, kernels, lr, activation, dr, batch_size, patience,
+         model_save_fn, pred_save_fn, train_years, valid_years, test_years, lead_time, gpu, iterative):
     os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu)
     # Limit TF memory usage
     limit_mem()
 
     # Open dataset and create data generators
-    # TODO: Flexible input data
-    z = xr.open_mfdataset(f'{datadir}geopotential_500/*.nc', combine='by_coords')
-    t = xr.open_mfdataset(f'{datadir}temperature_850/*.nc', combine='by_coords')
-    ds = xr.merge([z, t], compat='override')
+    ds = xr.merge([xr.open_mfdataset(f'{datadir}/{var}/*.nc', combine='by_coords') for var in var_dict.keys()])
 
-    # TODO: Flexible valid split
     ds_train = ds.sel(time=slice(*train_years))
     ds_valid = ds.sel(time=slice(*valid_years))
     ds_test = ds.sel(time=slice(*test_years))
 
-    dic = {var: None for var in vars}
-    dg_train = DataGenerator(ds_train, dic, lead_time, batch_size=batch_size)
-    dg_valid = DataGenerator(ds_valid, dic, lead_time, batch_size=batch_size, mean=dg_train.mean,
-                             std=dg_train.std, shuffle=False)
-    dg_test =  DataGenerator(ds_test, dic, lead_time, batch_size=batch_size, mean=dg_train.mean,
-                             std=dg_train.std, shuffle=False)
+    dg_train = DataGenerator(ds_train, var_dict, lead_time, batch_size=batch_size, output_vars=output_vars)
+    dg_valid = DataGenerator(ds_valid, var_dict, lead_time, batch_size=batch_size, mean=dg_train.mean,
+                             std=dg_train.std, shuffle=False, output_vars=output_vars)
+    dg_test =  DataGenerator(ds_test, var_dict, lead_time, batch_size=batch_size, mean=dg_train.mean,
+                             std=dg_train.std, shuffle=False, output_vars=output_vars)
     print(f'Mean = {dg_train.mean}; Std = {dg_train.std}')
 
     # Build model
     # TODO: Flexible input shapes and optimizer
-    model = build_cnn(filters, kernels, input_shape=(32, 64, len(vars)), activation=activation, dr=dr)
+    model = build_cnn(filters, kernels, input_shape=(32, 64, len(dg_train.data.level)), activation=activation, dr=dr)
     model.compile(keras.optimizers.Adam(lr), 'mse')
     print(model.summary())
 
@@ -237,16 +260,22 @@ def main(datadir, vars, filters, kernels, lr, activation, dr, batch_size, patien
 
 
     # Create predictions
-    pred = create_iterative_predictions(model, dg_test) if iterative else create_predictions(model, dg_test)
+    preds = create_predictions(model, dg_test)
     print(f'Saving predictions: {pred_save_fn}')
-    pred.to_netcdf(pred_save_fn)
+    preds.to_netcdf(pred_save_fn)
 
     # Print score in real units
     # TODO: Make flexible for other states
     z500_valid = load_test_data(f'{datadir}geopotential_500', 'z')
     t850_valid = load_test_data(f'{datadir}temperature_850', 't')
-    valid = xr.merge([z500_valid, t850_valid], compat='override')
-    print(evaluate_iterative_forecast(pred, valid).load() if iterative else compute_weighted_rmse(pred, valid).load())
+    try:
+        print(compute_weighted_rmse(preds.z.sel(level=500), z500_valid).load())
+    except:
+        print('Z500 not found in predictions.')
+    try:
+        print(compute_weighted_rmse(preds.t.sel(level=850), t850_valid).load())
+    except:
+        print('T850 not found in predictions.')
 
 if __name__ == '__main__':
     p = ArgParser()
@@ -254,7 +283,8 @@ if __name__ == '__main__':
     p.add_argument('--datadir', type=str, required=True, help='Path to data')
     p.add_argument('--model_save_fn', type=str, required=True, help='Path to save model')
     p.add_argument('--pred_save_fn', type=str, required=True, help='Path to save predictions')
-    p.add_argument('--vars', type=str, nargs='+', required=True, help='Variables')
+    p.add_argument('--var_dict', required=True, help='Variables: as an ugly dictionary...')
+    p.add_argument('--output_vars', nargs='+', help='Output variables. Format {var}_{level}', default=None)
     p.add_argument('--filters', type=int, nargs='+', required=True, help='Filters for each layer')
     p.add_argument('--kernels', type=int, nargs='+', required=True, help='Kernel size for each layer')
     p.add_argument('--lead_time', type=int, required=True, help='Forecast lead time')
@@ -273,7 +303,8 @@ if __name__ == '__main__':
 
     main(
         datadir=args.datadir,
-        vars=args.vars,
+        var_dict=ast.literal_eval(args.var_dict),
+        output_vars=args.output_vars,
         filters=args.filters,
         kernels=args.kernels,
         lr=args.lr,
