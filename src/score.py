a75fb3e35f62afa5469776a84ecd4a5e30897665
Sagar Garg <sagar@Sagars-MacBook-Air.local>
2020-04-06 09:22:45 +0200

    draft_changes_ignore

--- src/score.py
+++ src/score.py
@@ -3,6 +3,7 @@ Functions for evaluating forecasts.
 """
 import numpy as np
 import xarray as xr
+import properscoring as ps
 
 def load_test_data(path, var, years=slice('2017', '2018'), cmip=False):
     """
@@ -77,26 +78,25 @@ def compute_weighted_acc(da_fc, da_true):
     )
     return acc
 
-def compute_weighted_meanspread(prediction):
+def compute_weighted_meanspread(prediction,mean_dims=xr.ALL_DIMS):
     """
-    input: xarray. Coordinates: input_number, ensemble_number, lat, lon. Variables: z500, t850 or anything else
-    input_number: Let there be I initial conditions
-    ensemble_number: For each initial condition, let there be N forecasts (Toimprove: N can be differnet for differnet input_number)
+    prediction: xarray. Coordinates: time, forecast_number, lat, lon. Variables: z500, t850
+    time: Let there be I initial conditions
+    forecast_number: For each initial condition, let there be N forecasts
 
     #mean variance
     #1. for each input i, for each gridpoint, find variance among all N forecasts for that single input i
     #2. for each input i, find latitude-weighted average of all the lat*lon points
     #3. find average of all I inputs. take square root
     """
-    #ToDO: add assert condition to check for input size. Alternatively, if input does not have 'input_number' then add it as dimension
-    #ToDo: change from names like 'forecast_number' to dim=0 or dim=1 in order to generalize (Beneficial??)
-    var1=prediction.std('forecast_number')
+    #ToDO: add assert condition to check for input size. Alternatively, if input does not have 'time' then add it as dimension
+    var1=prediction.std('forecast_number') #is it okay to use a specific name like this (?)
     weights_lat = np.cos(np.deg2rad(var1.lat))
     weights_lat /= weights_lat.mean()
-    var2 =  (var1*weights_lat).mean(dim={'lat','lon'})
+    mean_spread= np.sqrt((var1*weights_lat).mean(mean_dims))
+    #var2 =  (var1*weights_lat).mean(dim={'lat','lon'})
     #var2=var1.mean(dim={'lat','lon'}) #without weighted latitude.
-    var3=var2.mean('input_number') #change to call it time/ valid_time?
-    mean_spread=np.sqrt(var3)
+    #mean_spread=np.sqrt(var2.mean('time'))
     
     if type(mean_spread) is xr.Dataset:
         mean_spread = mean_spread.rename({v: v + '_mean_spread' for v in mean_spread})
@@ -104,54 +104,35 @@ def compute_weighted_meanspread(prediction):
         mean_spread.name = error.name + '_mean_spread' if not error.name is None else 'mean_spread'
     return mean_spread
 
-def compute_weighted_meanrmse(observation,prediction):
-    """
-    similar to spread. 
-    1. for each input i, find mean of N forecasts:- ensmean
-    2. error=(ensmean-observation)**2
-    3. for each input i, find average error over grid.
-    4. find average among all I inputs
-    5. take square root
-    """
-    #computing mean rmse over all forecasts for all inputs
-    
-    ensmean1=prediction.mean('forecast_number')
-    error1=(ensmean1-observation)**2
-    weights_lat = np.cos(np.deg2rad(error1.lat))
-    weights_lat /= weights_lat.mean()
-    error2 = (error1* weights_lat).mean(dim={'lat','lon'})
-    #error2=error1.mean(dim={'lat','lon'}) #without weighted lat.
-    mean_rmse=np.sqrt(error2.mean('input_number'))
-    if type(mean_rmse) is xr.Dataset:
-            mean_rmse = mean_rmse.rename({v: v + '_mean_rmse' for v in mean_rmse})
-    else: # DataArray
-        mean_rmse.name = mean_rmse.name + '_mean_rmse' if not error.name is None else 'mean_rmse'
-    return mean_rmse
-
-def crps_score(observation,prediction,forecast_axis): 
+def crps_score(observation,prediction,forecast_axis,mean_dims=xr.ALL_DIMS): 
     #ToDo: improve argument that tells which dim is forecast_number
-    #ToDo: add weightd averaging in the code. so as to ouput just 1 value instead of a dataset.
-    import properscoring as ps
+    #import properscoring as ps
     obs = np.asarray(observation.to_array(), dtype=np.float32).squeeze();
-    #shape: (variable,input_number, lat, lon)
+    #shape: (variable,time, lat, lon)
     pred=np.asarray(prediction.to_array(), dtype=np.float32).squeeze();
-    #shape: (variable, forecast_number, input_number, lat, lon)
+    #shape: (variable, forecast_number, time, lat, lon)
     crps=ps.crps_ensemble(obs,pred, weights=None, issorted=False,axis=forecast_axis) 
-    #axis of forecast_number. crps.shape  #(variable, input_number, lat, lon)
+    #axis of forecast_number. crps.shape  #(variable, time, lat, lon)
     
     #Converting back to xarray
     crps_score = xr.Dataset({
         'z500': xr.DataArray(
         crps[0,...],
-        dims=['input_number', 'lat', 'lon'],
-        coords={'input_number': observation.input_number, 'lat': observation.lat, 'lon': observation.lon,
+        dims=['time', 'lat', 'lon'],
+        coords={'time': observation.time, 'lat': observation.lat, 'lon': observation.lon,
                 },
         ),
         't850': xr.DataArray(
         crps[1,...],
-        dims=['input_number', 'lat', 'lon'],
-        coords={'input_number': observation.input_number, 'lat': observation.lat, 'lon': observation.lon,
+        dims=['time', 'lat', 'lon'],
+        coords={'time': observation.time, 'lat': observation.lat, 'lon': observation.lon,
                 },
         )
     })
+    
+    #averaging to get single valye
+    weights_lat = np.cos(np.deg2rad(crps_score.lat))
+    weights_lat /= weights_lat.mean()
+    crps_score = (crps_score* weights_lat).mean(mean_dims)
+    
     return crps_score 
